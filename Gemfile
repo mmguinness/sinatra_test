64214c6c137720c6feed431321a9c7715b3b1b2c
Marie Mc Guinness <mcguinnessmarie@gmail.com>
2022-02-10 12:40:53 +0000

    Sinatra reloader added, to allow page refresh with changes

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ source 'https://rubygems.org'
 ruby '3.0.0'
 
 gem 'sinatra'
+gem 'sinatra-contrib'
--- hello.rb
+++ hello.rb
@@ -1,5 +1,18 @@
 require 'sinatra'
+require 'sinatra/reloader' if development?
 
 get '/' do
   'hello!'
 end
+
+get '/secret' do
+  'This is a secret page'
+end 
+
+get '/next_page' do
+  'This is the next page'
+end 
+
+get '/and_the_next_page' do
+  'This is the next next page'
+end 
