deff6ab89e2479e7b27332674c5b329862ba1296
Marie Mc Guinness <mcguinnessmarie@gmail.com>
2022-02-10 13:05:44 +0000

    Cat name and title added.

--- hello.rb
+++ hello.rb
@@ -2,9 +2,8 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 
 get '/' do
-  "<div>
-    <img src='http://placekitten.com/500/500'>
-  </div>"
+  @name = %w(Roman Mars).sample
+  erb :index
 end
 
 get '/secret' do
